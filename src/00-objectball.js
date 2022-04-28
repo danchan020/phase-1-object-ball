7dfc4ab582b41374b386536ed7916ae75756d49b
+ <diazcristina.la@gmail.com>
2022-04-28 09:31:35 -0700

    second commit

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -1,18 +1,20 @@
+const gameObject = () => ({
+  home: {
+    teamName: "Brooklyn Nets",
+    colors: ["black", "white"],
+    players: {
+      "Alan Anderson": {},
+      "Reggie Evans": {},
+      "Brook Lopez": {},
+      "Mason Plumlee": {},
+      "Jason Terry": {},
+    },
+  },
+  away: {
+    teamName: "",
+    colors: ["", ""],
+    players: {},
+  },
+});
 
-
-
-
-const gameObject = {
-        home: {
-            teamName: "",
-            colors: "",
-            players: "",
-    }
-        away: {
-            teamName: "",
-            colors: "",
-            players: "",
-    }  
-}
-
-console.log(gameObject)
+console.log(gameObject);
